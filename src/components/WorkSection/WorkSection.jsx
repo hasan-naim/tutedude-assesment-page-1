b15daf76fbcc6215e59273bb92432ccf31f69a59
hasan-naim <hasannaim6246@gmail.com>
2022-11-13 19:34:12 +0600

    work section is designed

--- src/components/WorkSection/WorkSection.jsx
+++ src/components/WorkSection/WorkSection.jsx
@@ -1,6 +1,44 @@
 import React from "react";
 import "./WorkSection.css";
 import groupPeople from "../../assets/icons/group-people.svg";
+import bookmark from "../../assets/icons/bookmark.svg";
+import moneySign from "../../assets/icons/moneySign.svg";
+import andPercent from "../../assets/icons/andpercent.svg";
+import wallet from "../../assets/icons/wallet.svg";
+import Card from "../Card/Card";
+
+const data = [
+  {
+    id: 1,
+    title: "Invite your Friends",
+    desc: "Share the link tutedude.com with your friends",
+    icon: groupPeople,
+  },
+  {
+    id: 2,
+    title: "Friend purcheases any course",
+    desc: "Using your Referral code in the payments page",
+    icon: bookmark,
+  },
+  {
+    id: 3,
+    title: "You get ₹ 200 as referal money",
+    desc: "On total purchase the friend makes, into your wallet",
+    icon: moneySign,
+  },
+  {
+    id: 4,
+    title: "Your friend gets ₹ 200 off",
+    desc: "On his overall fee on successful purchase using your referal code",
+    icon: andPercent,
+  },
+  {
+    id: 5,
+    title: "Transfer money from wallet",
+    desc: "When the wallet balance reaches ₹ 200 or more, you can withdraw it",
+    icon: wallet,
+  },
+];
 
 function WorkSection() {
   return (
@@ -8,15 +46,9 @@ function WorkSection() {
       <div className="container">
         <h1>How does it work ?</h1>
         <div className="cards-container">
-          <div className="card flex">
-            <div className="card-img">
-              <img src={groupPeople} alt="" />
-            </div>
-            <div className="card-desc">
-              <h4>Invite your Friends</h4>
-              <p>Share the link tutedude.com with your friends</p>
-            </div>
-          </div>
+          {data.map((obj) => (
+            <Card key={obj.id} data={obj} />
+          ))}
         </div>
       </div>
     </section>
